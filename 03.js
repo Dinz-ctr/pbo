c37f99085c77a8b23be88877d9932961fc192f53
Dinz-ctr <fhudin121@gmail.com>
2025-10-04 11:20:51 +0700

    Create 03.js

---
+++ 03.js
@@ -0,0 +1,15 @@
+class Mahasiswa {
+  constructor(nama, nim, jurusan) {
+    this.nama = nama;
+    this.nim = nim;
+    this.jurusan = jurusan;
+  }
+
+  belajar() {
+    console.log(${this.nama} sedang belajar.);
+  }
+}
+
+// Membuat objek dari class Mahasiswa
+const mhs1 = new Mahasiswa("Satura", "23012345", "Teknik Informatika");
+mhs1.belajar();
